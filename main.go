8a920cb63f7a3a1f3bf214dc1a03b9b641bece39
Michael Thaler <michael.thaler2@gmail.com>
2025-12-11 07:53:50 +0100

    Updated main.go.

--- main.go
+++ main.go
@@ -1,4 +1,11 @@
 package main
 
 func main() {
+	fName := "heights.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
+	defer file.Close()
 }
