1c391214ed2d2b6e188c304533a8e8134c492713
Michael Thaler <michael.thaler2@gmail.com>
2025-12-11 07:55:49 +0100

    Updated main.go.

--- main.go
+++ main.go
@@ -8,4 +8,16 @@ func main() {
 		return
 	}
 	defer file.Close()
+
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: coursera.org, www.coursera.org
+		colly.AllowedDomains("https://www.deine-berge.de"),
+
+		// Cache responses to prevent multiple download of pages
+		// even if the collector is restarted
+		colly.CacheDir("./height_cache"),
+		// Cached responses older than the specified duration will be refreshed
+		colly.CacheExpiration(24*time.Hour),
+	)
 }
