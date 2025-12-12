23758b32823d4811c98d4d4ed1a1a23fbb680f79
Michael Thaler <michael.thaler2@gmail.com>
2025-12-12 06:51:46 +0100

    Updated summit.go.

--- summit.go
+++ summit.go
@@ -3,10 +3,24 @@ package main
 // Summit stores information about a summit
 type Summit struct {
 	Name        string
-	Height      int
+	Height      string
 	Kategorie   string
 	Staat       string
 	Region		string
 	Gruppe		string
 	Information string
+}
+
+
+func (s Summit) toSlice []strinh {
+	var result []string
+	result = make([]string, 8)
+	result = append(result, s.Name)
+	result = append(result, s.Height)
+	result = append(result, s.Kategorie)
+	result = append(result, s.Staat)
+	result = append(result, s.Region)
+	result = append(result, s.Gruppe)
+	result = append(result, s.Information)
+	return ressult
 }
